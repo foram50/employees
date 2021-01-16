8c17a4e10f0bb31bf8185caf3f3f59148c26a9dc
unknown <foramsapariya12345@gmail.com>
2021-01-16 19:30:57 +0530

    controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Mausmi
+                    foraam
                 </div>
 
                 <div class="links">
